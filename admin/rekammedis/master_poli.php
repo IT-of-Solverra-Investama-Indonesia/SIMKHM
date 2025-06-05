f898a3afafc71902324d6b7f03de570356c8731f
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2025-06-05 14:29:45 +0700

    Membuat master poli untuk mengcopy ke lokal pada rekam medis

--- admin/rekammedis/master_poli.php
+++ admin/rekammedis/master_poli.php
@@ -1,9 +1,6 @@
 <?php
-function getFullUrl()
-{
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ||
-        $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-
+function getFullUrl(){
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
     return $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 }
 $arrayListHalamanLain = array('tambahMaster', 'editMaster', 'deleteMaster');
