dfa5074df6d6a5b79c96521a494467fcd5845901
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2025-07-21 11:56:40 +0700

    menambahkan fungsi sanitasi pada function sebagai clear Injection SQL

--- admin/dist/function.php
+++ admin/dist/function.php
@@ -1,6 +1,12 @@
 <?php 
     $koneksi = mysqli_connect("localhost", "root", "", "klinik");
     $koneksimaster = mysqli_connect("localhost", "root", "", "klinik_master");
-    // $koneksidoc = mysqli_connect("localhost", "root", "", "dokumen");
+// $koneksidoc = mysqli_connect("localhost", "root", "", "dokumen");
+
+
+function sani($data)
+{
+    return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
+}
 ?>
 
