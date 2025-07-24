c878aedb080e16eb5d3747c19d7f57730b78f71c
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2025-07-24 09:15:32 +0700

    # Menambah rata rata obat pasien umum per dokter per bulan pada dashboard
    [Menambah rata rata obat pasien malam per dokter per bulan pada dashboard](https://www.notion.so/Menambah-rata-rata-obat-pasien-malam-per-dokter-per-bulan-pada-dashboard-23a0f645e24e801e80d7f65ce057ade4?pvs=21)
    
    [Menambah rata rata obat pasien gigi umum per dokter per bulan pada dashboard](https://www.notion.so/Menambah-rata-rata-obat-pasien-gigi-umum-per-dokter-per-bulan-pada-dashboard-23a0f645e24e8076ad11fa2010238373?pvs=21)
    
    [Menambah rata rata obat pasien gigi bpjs per dokter per bulan pada dashboard](https://www.notion.so/Menambah-rata-rata-obat-pasien-gigi-bpjs-per-dokter-per-bulan-pada-dashboard-23a0f645e24e805c9da1dc7c00d1ef89?pvs=21)
    
    [Menambah rata rata obat pasien spesialis anak per dokter per bulan pada dashboard](https://www.notion.so/Menambah-rata-rata-obat-pasien-spesialis-anak-per-dokter-per-bulan-pada-dashboard-23a0f645e24e8031a8c0ceb9d0394318?pvs=21)
    
    [Menambah rata rata obat pasien spesialis penyakit dalam per dokter per bulan pada dashboard](https://www.notion.so/Menambah-rata-rata-obat-pasien-spesialis-penyakit-dalam-per-dokter-per-bulan-pada-dashboard-23a0f645e24e808db9a3fe9a55ea25fb?pvs=21)
    
    [Menambah rata rata obat pasien kosmetik per dokter per bulan pada dashboard](https://www.notion.so/Menambah-rata-rata-obat-pasien-kosmetik-per-dokter-per-bulan-pada-dashboard-23a0f645e24e8040b327c57c86a0cc87?pvs=21)

--- admin/dashboard/dashboard_detail_rataObatPasienBPJSperBulan.php
+++ admin/dashboard/dashboard_detail_rataObatPasienBPJSperBulan.php
@@ -7,6 +7,29 @@
                 <input type="month" class="form-control form-control-sm" id="bulan" name="bulan"
                     value="<?= $bulanSaatIni = (isset($_GET['bulan']) ? htmlspecialchars($_GET['bulan']) : date('Y-m')) ?>">
             </div>
+            <div class="col-5">
+                <?php
+                $carabayar = isset($_GET['carabayar']) ? htmlspecialchars($_GET['carabayar']) : 'bpjs';
+                if ($carabayar == 'all') {
+                    $whereCaraBayar = "AND (carabayar IN ('bpjs', 'umum', 'malam', 'gigi umum', 'gigi bpjs', 'spesialis anak', 'spesialis penyakit dalam', 'kosmetik'))";
+                    $whereCaraBayarR = "AND (r.carabayar IN ('bpjs', 'umum', 'malam', 'gigi umum', 'gigi bpjs', 'spesialis anak', 'spesialis penyakit dalam', 'kosmetik'))";
+                } else {
+                    $whereCaraBayar = "AND carabayar = '$carabayar'";
+                    $whereCaraBayarR = "AND r.carabayar = '$carabayar'";
+                }
+                ?>
+                <select name="carabayar" class="form-select form-select-sm" id="">
+                    <option value="all" <?= $carabayar == 'all' ? 'selected' : '' ?>>All</option>
+                    <option value="bpjs" <?= $carabayar == 'bpjs' ? 'selected' : '' ?>>BPJS</option>
+                    <option value="umum" <?= $carabayar == 'umum' ? 'selected' : '' ?>>Umum</option>
+                    <option value="malam" <?= $carabayar == 'malam' ? 'selected' : '' ?>>Malam</option>
+                    <option value="gigi umum" <?= $carabayar == 'gigi umum' ? 'selected' : '' ?>>Gigi Umum</option>
+                    <option value="gigi bpjs" <?= $carabayar == 'gigi bpjs' ? 'selected' : '' ?>>Gigi BPJS</option>
+                    <option value="spesialis anak" <?= $carabayar == 'spesialis anak' ? 'selected' : '' ?>>Spesialis Anak</option>
+                    <option value="spesialis penyakit dalam" <?= $carabayar == 'spesialis penyakit dalam' ? 'selected' : '' ?>>Spesialis Penyakit Dalam</option>
+                    <option value="kosmetik" <?= $carabayar == 'kosmetik' ? 'selected' : '' ?>>Kosmetik</option>
+                </select>
+            </div>
             <div class="col-2 align-self-end">
                 <button type="submit" class="btn btn-primary btn-sm w-100"><i class="bi bi-search"></i></button>
             </div>
@@ -14,7 +37,7 @@
     </form>
 </div>
 <div class="card shadow p-2">
-    <h5 class="card-title mb-0 mt-0">Rata Rata Obat Pasien BPJS Per-Bulan</h5>
+    <h5 class="card-title mb-0 mt-0 text-capitalize">Rata Rata Obat Pasien <?= $carabayar ?> Per-Bulan</h5>
     <div class="table-responsive">
         <table class="table table-hover table-striped table-sm" style="font-size: 12px;">
             <thead>
@@ -38,7 +61,7 @@
                     COUNT(*) as jumlahpasien
                 FROM registrasi_rawat
                 WHERE perawatan = 'Rawat Jalan' 
-                    AND carabayar = 'bpjs' 
+                    " . $whereCaraBayar . " 
                     AND status_antri != 'Belum Datang'
                     AND DATE_FORMAT(jadwal, '%Y-%m') = '$bulanSaatIni'
                 GROUP BY DATE_FORMAT(jadwal, '%Y-%m'), dokter_rawat
@@ -68,7 +91,7 @@
                     )
                 ) a ON a.id_obat = o.kode_obat
                 WHERE r.perawatan = 'Rawat Jalan'
-                    AND r.carabayar = 'bpjs'
+                    " . $whereCaraBayarR . "
                     AND r.status_antri != 'Belum Datang'
                     AND o.rekam_medis_id IS NOT NULL
                     AND DATE_FORMAT(r.jadwal, '%Y-%m') = '$bulanSaatIni'
@@ -89,7 +112,7 @@
                     $bulan = $data['bulan'] == date('Y-m') ? date('Y-m') : $data['bulan'];
                     $dokter = $data['dokter_rawat'];
                     // $hari = date('t', mktime(0, 0, 0, substr($bulan, 0, 2), 1, substr($bulan, 3)));
-                    $hari = date('t', mktime(0, 0, 0, (int)substr($bulan, 0, 2), 1, (int)substr($bulan, 3)));
+                    $hari = $bulan == date('Y-m') ? date('d') : date('t', mktime(0, 0, 0, (int)substr($bulan, 0, 2), 1, (int)substr($bulan, 3)));
 
                     $key = $bulan . '|' . $dokter;
                     $total = 0;
