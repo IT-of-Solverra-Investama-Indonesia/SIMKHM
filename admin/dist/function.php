8a5dd2ecbe8d272a996432a787511d97f854844d
Evan0404 <evanlum4jang@gmail.com>
2025-11-08 10:54:34 +0700

    Refactor database connection logic in function.php to dynamically set connections based on URL segments

--- admin/dist/function.php
+++ admin/dist/function.php
@@ -1,8 +1,54 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "klinik_wonorejo");
-    $koneksimaster = mysqli_connect("localhost", "root", "", "klinik_master");
-// $koneksidoc = mysqli_connect("localhost", "root", "", "dokumen");
 
+// Deteksi URL dan set string berdasarkan lokasi
+$currentURL = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] : '';
+$currentURL = strtolower($currentURL); // Convert to lowercase untuk case-insensitive
+
+$string = ''; // Default value
+if(strpos($currentURL, 'ermkhm') !== false){
+    if (strpos($currentURL, 'wonorejo') !== false) {
+        $koneksi = mysqli_connect("localhost", "ermkhmid_wonorejo", "W5VAkyndC8GEBMDrRH7F", "ermkhmid_wonorejo");
+        $koneksimaster = mysqli_connect("localhost", "ermkhmid_master", "5pa3D84p5kerQLVRjCR5", "ermkhmid_master");
+    } elseif (strpos($currentURL, 'klakah') !== false) {
+        $koneksi = mysqli_connect("localhost", "ermkhmid_klakah", "8JCL5nx39aKnSLtLgG6Q", "ermkhmid_klakah");
+        $koneksimaster = mysqli_connect("localhost", "ermkhmid_master", "5pa3D84p5kerQLVRjCR5", "ermkhmid_master");
+    } elseif (strpos($currentURL, 'tunjung') !== false) {
+        $koneksi = mysqli_connect("localhost", "ermkhmid_tunjung", "2fK6K6xg4LshSTLBTMJG", "ermkhmid_tunjung");
+        $koneksimaster = mysqli_connect("localhost", "ermkhmid_master", "5pa3D84p5kerQLVRjCR5", "ermkhmid_master");
+    } elseif (strpos($currentURL, 'kunir') !== false) {
+        $koneksi = mysqli_connect("localhost", "ermkhmid_kunir", "Fq65VjpxzKa4A7rZaXBN", "ermkhmid_kunir");
+        $koneksimaster = mysqli_connect("localhost", "ermkhmid_master", "5pa3D84p5kerQLVRjCR5", "ermkhmid_master");
+    }else{
+        $koneksi = mysqli_connect("localhost", "ermkhmid_wonorejo", "W5VAkyndC8GEBMDrRH7F", "ermkhmid_wonorejo");
+        $koneksimaster = mysqli_connect("localhost", "ermkhmid_master", "5pa3D84p5kerQLVRjCR5", "ermkhmid_master");
+    }
+}elseif(strpos($currentURL, 'simkhm') !== false){
+    if (strpos($currentURL, 'wonorejo') !== false) {
+        $koneksi = mysqli_connect("103.28.12.123", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_database");
+        $koneksimaster = mysqli_connect("localhost", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_master");
+    } elseif (strpos($currentURL, 'klakah') !== false) {
+        $koneksi = mysqli_connect("103.28.12.123", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_klakah");
+        $koneksimaster = mysqli_connect("localhost", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_master");
+    } elseif (strpos($currentURL, 'tunjung') !== false) {
+        $koneksi = mysqli_connect("103.28.12.123", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_tunjung");
+        $koneksimaster = mysqli_connect("localhost", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_master");
+    } elseif (strpos($currentURL, 'kunir') !== false) {
+        $koneksi = mysqli_connect("103.28.12.123", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_kunir");
+        $koneksimaster = mysqli_connect("localhost", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_master");
+    }else{
+        $koneksi = mysqli_connect("103.28.12.123", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_database");
+        $koneksimaster = mysqli_connect("localhost", "simkhmid_simkhmuser", "Wijaya1702!", "simkhmid_master");
+        if (!$koneksi) {
+            // Jika koneksi gagal, fallback ke database lokal klinik
+            $koneksi = mysqli_connect("localhost", "root", "", "klinik");
+            $koneksimaster = mysqli_connect("localhost", "root", "", "klinik_master");
+        }
+
+    }
+}else{
+    $koneksi = mysqli_connect("localhost", "root", "", "klinik");
+    $koneksimaster = mysqli_connect("localhost", "root", "", "klinik_master");
+}
 
 function sani($data)
 {
