161d9ebac3d256e7589eaf491e2393b8071ddfa7
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2025-11-10 15:05:23 +0700

    Refactor base URL assignment logic to dynamically set URLs based on the current request

--- admin/dist/baseUrlAPI.php
+++ admin/dist/baseUrlAPI.php
@@ -1,3 +1,37 @@
 <?php
-$baseUrlSIMKHM = "https://simkhm.id/tunjung/";
-$baseUrlLama = "https://husadamulia.com/wonorejo/";
+$currentURL = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] : '';
+$currentURL = strtolower($currentURL);
+if (strpos($currentURL, 'simkhm') !== false) {
+    if (strpos($currentURL, 'wonorejo') !== false) {
+        $baseUrlSIMKHM = "https://simkhm.id/wonorejo/";
+        $baseUrlLama = "https://husadamulia.com/wonorejo/";
+    } elseif (strpos($currentURL, 'klakah') !== false) {
+        $baseUrlSIMKHM = "https://simkhm.id/klakah/";
+        $baseUrlLama = "https://husadamulia.com/klakah/";
+    } elseif (strpos($currentURL, 'tunjung') !== false) {
+        $baseUrlSIMKHM = "https://simkhm.id/tunjung/";
+        $baseUrlLama = "https://husadamulia.com/tunjung/";
+    } else {
+        $baseUrlSIMKHM = "https://simkhm.id/kunir/";
+        $baseUrlLama = "https://husadamulia.com/kunir/";
+    }
+}else if (strpos($currentURL, 'ermkhm') !== false){
+    if (strpos($currentURL, 'wonorejo') !== false) {
+        $baseUrlSIMKHM = "https://ermkhm.id/wonorejo/";
+        $baseUrlLama = "https://husadamulia.com/wonorejo/";
+    } elseif (strpos($currentURL, 'klakah') !== false) {
+        $baseUrlSIMKHM = "https://ermkhm.id/klakah/";
+        $baseUrlLama = "https://husadamulia.com/klakah/";
+    } elseif (strpos($currentURL, 'tunjung') !== false) {
+        $baseUrlSIMKHM = "https://ermkhm.id/tunjung/";
+        $baseUrlLama = "https://husadamulia.com/tunjung/";
+    } else {
+        $baseUrlSIMKHM = "https://ermkhm.id/kunir/";
+        $baseUrlLama = "https://husadamulia.com/kunir/";
+    }
+}else{
+    $baseUrlSIMKHM = "https://simkhm.id/kunir/";
+    $baseUrlLama = "https://husadamulia.com/kunir/";
+}
+
+
