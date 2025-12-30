04e425000be666dcf4a3391351c80b6c2352e7ef
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2025-12-30 15:57:21 +0700

    - Membuat halaman dashboard baru untuk monitoring kinerja staff rawat inap - Menampilkan 2 kategori rekap petugas: Perawatan Kajian Awal Berdasarkan tag pada tabel kajian_awal_inap_tag, Perawatan IGD Berdasarkan data perawat pada registrasi rawat - Fitur filter berdasarkan periode bulan (dari bulan - hingga bulan) - Tampilan jumlah pasien per petugas per bulan - Link detail untuk melihat daftar pasien yang ditangani setiap petugas - Tombol Repair Shift Inap untuk perbaikan data shift otomatis berdasarkan waktu input - Menampilkan detail pasien yang ditangani oleh petugas tertentu - Informasi lengkap: Nama Pasien, Jadwal, Waktu Input Kajian, Dokter yang menangani - Pemisahan detail untuk perawatan kajian awal dan perawatan IGD - Tombol kembali untuk navigasi yang mudah

--- admin/dashboard/dashboardstaff_rekapStaffInap.php
+++ admin/dashboard/dashboardstaff_rekapStaffInap.php
@@ -1,89 +1,201 @@
 <div>
-    <h5>Rekap Staff Inap</h5>
-    <?php if (isset($_GET['ITSolverra'])) { ?>
-        <?php
-        $ambilAllUrl = $_SERVER['REQUEST_URI'];
-        // $ambilAllUrl = strtok($_SERVER['REQUEST_URI'], '&repairShiftInap');
+    <?php if (!isset($_GET['detailPetugas'])) { ?>
+        <div>
+            <h5>Rekap Staff Inap</h5>
+            <?php if (isset($_GET['ITSolverra'])) { ?>
+                <?php
+                $ambilAllUrl = $_SERVER['REQUEST_URI'];
+                // $ambilAllUrl = strtok($_SERVER['REQUEST_URI'], '&repairShiftInap');
 
-        if (isset($_GET['repairShiftInap'])) {
-            // Query untuk memperbaiki data shift inap
-            $koneksi->query("UPDATE rawatinapdetail SET shiftinap = CASE 
-                WHEN TIME(created_at) BETWEEN '07:00:00' AND '14:00:00' THEN 'Pagi'
-                WHEN TIME(created_at) BETWEEN '14:00:01' AND '21:00:00' THEN 'Sore'
-                ELSE 'Malam'
-            END
-            WHERE shiftinap IS NULL OR shiftinap = ''");
-            // -- WHERE 1=1");
-            echo "
-                <script>
-                    alert('Data shift inap telah diperbaiki.');
-                    window.location.href = 'index.php?halaman=dashboardstaff&tipe=rekapStaffInap&ITSolverra';
-                </script>
-            ";
-        }
-
-        ?>
-        <a href="<?= $ambilAllUrl ?>&repairShiftInap" class="btn btn-sm btn-success mb-2">Repair Shift Inap</a>
-    <?php } ?>
-    <div class="card shadow p-2 mb-2">
-        <form method="get">
+                if (isset($_GET['repairShiftInap'])) {
+                    // Query untuk memperbaiki data shift inap
+                    $koneksi->query("UPDATE rawatinapdetail SET shiftinap = CASE 
+                        WHEN TIME(created_at) BETWEEN '07:00:00' AND '14:00:00' THEN 'Pagi'
+                        WHEN TIME(created_at) BETWEEN '14:00:01' AND '21:00:00' THEN 'Sore'
+                        ELSE 'Malam'
+                    END
+                    WHERE shiftinap IS NULL OR shiftinap = ''");
+                    // -- WHERE 1=1");
+                    echo "
+                        <script>
+                            alert('Data shift inap telah diperbaiki.');
+                            window.location.href = 'index.php?halaman=dashboardstaff&tipe=rekapStaffInap&ITSolverra';
+                        </script>
+                    ";
+                }
+                ?>
+                <a href="<?= $ambilAllUrl ?>&repairShiftInap" class="btn btn-sm btn-success mb-2">Repair Shift Inap</a>
+            <?php } ?>
+            <div class="card shadow p-2 mb-2">
+                <form method="get">
+                    <div class="row g-1">
+                        <input type="text" name="halaman" value="dashboardstaff" hidden id="">
+                        <input type="text" name="tipe" value="rekapStaffInap" hidden id="">
+                        <div class="col-5">
+                            <input type="month" value="<?= $month_start = (isset($_GET['month_start']) ? htmlspecialchars($_GET['month_start']) : date('Y-m')) ?>" onblur="this.type='text'" onfocus="this.type='month'" placeholder="Dari Bulan" name="month_start" class="form-control form-control-sm" id="">
+                        </div>
+                        <div class="col-5">
+                            <input type="month" value="<?= $month_end = (isset($_GET['month_end']) ? htmlspecialchars($_GET['month_end']) : date('Y-m')) ?>" onblur="this.type='text'" onfocus="this.type='month'" placeholder="Hingga Bulan" name="month_end" class="form-control form-control-sm" id="">
+                        </div>
+                        <div class="col-2">
+                            <button class="btn btn-sm btn-primary"><i class="bi bi-search"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div class="row g-1">
-                <input type="text" name="halaman" value="dashboardstaff" hidden id="">
-                <input type="text" name="tipe" value="rekapStaffInap" hidden id="">
-                <div class="col-5">
-                    <input type="month" value="<?= $month_start = (isset($_GET['month_start']) ? htmlspecialchars($_GET['month_start']) : date('Y-m')) ?>" onblur="this.type='text'" onfocus="this.type='month'" placeholder="Dari Bulan" name="month_start" class="form-control form-control-sm" id="">
+                <div class="col-md-6">
+                    <div class="card shadow p-2">
+                        <h6><b>Perawatan</b></h6>
+                        <table class="table table-striped table-hover table-sm" style="font-size: 12px;">
+                            <thead>
+                                <tr>
+                                    <th>Bulan</th>
+                                    <th>Petugas</th>
+                                    <th>Pasien</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $totalPasien = 0;
+                                $getData = $koneksi->query("SELECT DATE_FORMAT(rr.jadwal, '%M %Y') as Bulan, kj.petugas as Petugas, COUNT(rr.no_rm) as jumlahPasien FROM kajian_awal_inap_tag kj INNER JOIN registrasi_rawat rr ON rr.idrawat = kj.idrawat WHERE DATE_FORMAT(rr.jadwal, '%Y-%m') >= '$month_start' AND DATE_FORMAT(rr.jadwal, '%Y-%m') <= '$month_end' AND rr.perawatan = 'Rawat Inap' GROUP BY DATE_FORMAT(rr.jadwal, '%Y-%m'), kj.petugas ORDER BY DATE_FORMAT(rr.jadwal, '%Y-%m') DESC, kj.petugas ASC");
+                                foreach ($getData as $data) {
+                                ?>
+                                    <tr>
+                                        <td><?= $data['Bulan'] ?></td>
+                                        <td>
+                                            <?= $data['Petugas'] ?? "" ?>
+                                        </td>
+                                        <td>
+                                            <a href="?halaman=dashboardstaff&tipe=rekapStaffInap&month_start=<?= $month_start ?>&month_end=<?= $month_end ?>&detailPetugas=<?= $data['Petugas'] ?>" class="badge bg-warning" style="font-size: 12px;">
+                                                <?= $data['jumlahPasien'] ?> Pasien
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    $totalPasien += $data['jumlahPasien'];
+                                    ?>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div class="col-5">
-                    <input type="month" value="<?= $month_end = (isset($_GET['month_end']) ? htmlspecialchars($_GET['month_end']) : date('Y-m')) ?>" onblur="this.type='text'" onfocus="this.type='month'" placeholder="Hingga Bulan" name="month_end" class="form-control form-control-sm" id="">
-                </div>
-                <div class="col-2">
-                    <button class="btn btn-sm btn-primary"><i class="bi bi-search"></i></button>
+                <div class="col-md-6">
+                    <div class="card shadow p-2">
+                        <h6><b>Perawatan IGD</b></h6>
+                        <table class="table table-striped table-hover table-sm" style="font-size: 12px;">
+                            <thead>
+                                <tr>
+                                    <th>Bulan</th>
+                                    <th>Petugas</th>
+                                    <th>Pasien</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $totalPasienIGD = 0;
+                                $getDataIGD = $koneksi->query("SELECT DATE_FORMAT(jadwal, '%M %Y') as Bulan, perawat as Petugas, COUNT(no_rm) as jumlahPasien FROM registrasi_rawat WHERE perawatan = 'Rawat Inap' AND DATE_FORMAT(jadwal, '%Y-%m') >= '$month_start' AND DATE_FORMAT(jadwal, '%Y-%m') <= '$month_end' GROUP BY DATE_FORMAT(jadwal, '%Y-%m'), perawat ORDER BY DATE_FORMAT(jadwal, '%Y-%m') DESC, perawat ASC");
+                                foreach ($getDataIGD as $dataIGD) {
+                                ?>
+                                    <tr>
+                                        <td><?= $dataIGD['Bulan'] ?></td>
+                                        <td>
+                                            <?= $dataIGD['Petugas'] ?? "" ?>
+                                        </td>
+                                        <td>
+                                            <a href="?halaman=dashboardstaff&tipe=rekapStaffInap&month_start=<?= $month_start ?>&month_end=<?= $month_end ?>&detailPetugas=<?= $dataIGD['Petugas'] ?>&IGD" class="badge bg-warning" style="font-size: 12px;">
+                                                <?= $dataIGD['jumlahPasien'] ?> Pasien
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    $totalPasienIGD += $dataIGD['jumlahPasien'];
+                                    ?>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </form>
-    </div>
-    <div class="card shadow p-2">
-        <table class="table table-striped table-hover table-sm" style="font-size: 12px;">
-            <thead>
-                <tr>
-                    <th>Bulan</th>
-                    <th>Petugas</th>
-                    <th>Shift</th>
-                    <th>Pasien</th>
-                    <th>Pasien/Shift</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $getData = $koneksi->query("SELECT *, DATE_FORMAT(tgl, '%M %Y') as bulan, petugas FROM rawatinapdetail WHERE DATE_FORMAT(tgl, '%Y-%m') BETWEEN '$month_start' AND '$month_end' GROUP BY DATE_FORMAT(tgl, '%M %Y'), petugas ORDER BY created_at DESC");
-                foreach ($getData as $data) {
-                ?>
-                    <tr>
-                        <td><?= $data['bulan'] ?></td>
-                        <td>
+        </div>
+    <?php } else { ?>
+        <?php
+        $month_start = isset($_GET['month_start']) ? htmlspecialchars($_GET['month_start']) : date('Y-m');
+        $month_end = isset($_GET['month_end']) ? htmlspecialchars($_GET['month_end']) : date('Y-m');
+        $detailPetugas = htmlspecialchars($_GET['detailPetugas']);
+        ?>
+        <?php if (!isset($_GET['IGD'])) { ?>
+            <div class="card shadow p-2 mt-0">
+                <div>
+                    <a href="?halaman=dashboardstaff&tipe=rekapStaffInap&month_start=<?= $month_start ?>&month_end=<?= $month_end ?>" class="btn btn-sm btn-secondary mb-2"><i class="bi bi-arrow-left"></i> Kembali</a>
+                </div>
+                <h6><b>Detail Rekap Staff Inap <?= $detailPetugas ?> Pada <?= $month_start ?> Sampai <?= $month_end ?></b></h6>
+                <div class="table-responsive">
+                    <table class="table table-sm table-hover table-striped" style="font-size: 11px;">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Petugas</th>
+                                <th>Pasien</th>
+                                <th>Jadwal Pasien</th>
+                                <th>Input Kajian Pada</th>
+                                <th>Dokter</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             <?php
-                            $getUser = $koneksi->query("SELECT * FROM admin WHERE namalengkap = '$data[petugas]' OR username = '$data[petugas]'")->fetch_assoc();
+                            $no = 1;
+                            $getDetail = $koneksi->query("SELECT DATE_FORMAT(rr.jadwal, '%M %Y') as Bulan, kj.petugas as Petugas, rr.nama_pasien, rr.jadwal, kj.created_at as tglCatatan, rr.dokter_rawat FROM kajian_awal_inap_tag kj INNER JOIN registrasi_rawat rr ON rr.idrawat = kj.idrawat WHERE DATE_FORMAT(rr.jadwal, '%Y-%m') >= '$month_start' AND DATE_FORMAT(rr.jadwal, '%Y-%m') <= '$month_end' AND rr.perawatan = 'Rawat Inap' AND kj.petugas = '$detailPetugas' ORDER BY DATE_FORMAT(rr.jadwal, '%Y-%m-%d') DESC");
+                            foreach ($getDetail as $detail) {
                             ?>
-                            <?= $getUser['namalengkap'] ?? "" ?>
-                        </td>
-                        <td>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $detail['Petugas'] ?></td>
+                                    <td><?= $detail['nama_pasien'] ?></td>
+                                    <td><?= $detail['jadwal'] ?></td>
+                                    <td><?= $detail['tglCatatan'] ?></td>
+                                    <td><?= $detail['dokter_rawat'] ?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        <?php } else { ?>
+            <div class="card shadow p-2 mt-0">
+                <div>
+                    <a href="?halaman=dashboardstaff&tipe=rekapStaffInap&month_start=<?= $month_start ?>&month_end=<?= $month_end ?>" class="btn btn-sm btn-secondary mb-2"><i class="bi bi-arrow-left"></i> Kembali</a>
+                </div>
+                <h6><b>Detail Rekap Staff IGD <?= $detailPetugas ?> Pada <?= $month_start ?> Sampai <?= $month_end ?></b></h6>
+                <div class="table-responsive">
+                    <table class="table table-sm table-hover table-striped" style="font-size: 11px;">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Petugas</th>
+                                <th>Pasien</th>
+                                <th>Jadwal Pasien</th>
+                                <th>Dokter</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             <?php
-                            $jumlahShift = $getJumlahShift = $koneksi->query("SELECT * FROM rawatinapdetail WHERE DATE_FORMAT(tgl, '%Y-%m') = DATE_FORMAT('$data[tgl]', '%Y-%m') AND petugas = '$data[petugas]' GROUP BY DATE_FORMAT(tgl, '%Y-%m-%d'), shiftinap")->num_rows;
+                            $no = 1;
+                            $getDetail = $koneksi->query("SELECT DATE_FORMAT(jadwal, '%M %Y') as Bulan, perawat as Petugas, nama_pasien, jadwal, dokter_rawat FROM registrasi_rawat WHERE perawatan = 'Rawat Inap' AND DATE_FORMAT(jadwal, '%Y-%m') >= '$month_start' AND DATE_FORMAT(jadwal, '%Y-%m') <= '$month_end' AND perawat = '$detailPetugas' ORDER BY DATE_FORMAT(jadwal, '%Y-%m-%d') DESC");
+                            foreach ($getDetail as $detail) {
                             ?>
-                            <?= $jumlahShift ?>x Jaga
-                        </td>
-                        <td>
-                            <?php
-                            $jumlahPasien = $koneksi->query("SELECT * FROM rawatinapdetail WHERE DATE_FORMAT(tgl, '%Y-%m') = DATE_FORMAT('$data[tgl]', '%Y-%m') AND petugas = '$data[petugas]' GROUP BY id")->num_rows;
-                            ?>
-                            <?= $jumlahPasien ?> Pasien
-                        </td>
-                        <td>
-                            <?= number_format($jumlahPasien / $jumlahShift, 2) ?>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $detail['Petugas'] ?></td>
+                                    <td><?= $detail['nama_pasien'] ?></td>
+                                    <td><?= $detail['jadwal'] ?></td>
+                                    <td><?= $detail['dokter_rawat'] ?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        <?php } ?>
+    <?php } ?>
 </div>
