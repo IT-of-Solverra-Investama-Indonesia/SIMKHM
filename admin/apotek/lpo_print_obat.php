05669fb732b1506c7458a69d4fcb3696947ff451
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2026-02-04 18:13:41 +0700

    - Memberikan 3 pilihan print saat print obat ranap, E-resep, Kasir, dan Default - Print Obat akan menjadi e-resep jika di klik e-resep, yang berarti menghilangkan harga harga yang tertera - Print obat akan menjadi nota jika di klik kasir, yang berarti cara penggunaan akan tidak ada, hanya jumlah dan harga serta total - Jika di klik default maka aka muncul semua nya, termasuk dari cara penggunakan dan juga harga - Memberikan kondisi jika saat halaman ini di print pilihan e-resep kasir dan default akan hilang agar tidak ikut ter print

--- admin/apotek/lpo_print_obat.php
+++ admin/apotek/lpo_print_obat.php
@@ -19,11 +19,22 @@ $getPetugasSingle = $koneksi->query("SELECT * FROM obat_rm  WHERE idrm = '$_GET[
         font-family: monospace;
         /* letter-spacing: px; */
     }
+
+    @media print {
+        #button {
+            display: none;
+        }
+    }
 </style>
+<div id="button">
+    <a href="lpo_print_obat.php?id=<?= $_GET['id'] ?>&inap&tgl=<?= $_GET['tgl'] ?>&tglObat=<?= $_GET['tglObat'] ?>&jenis=<?= $_GET['jenis'] ?>&eresep">E-Resep</a>
+    <a href="lpo_print_obat.php?id=<?= $_GET['id'] ?>&inap&tgl=<?= $_GET['tgl'] ?>&tglObat=<?= $_GET['tglObat'] ?>&jenis=<?= $_GET['jenis'] ?>&kasir">Kasir</a>
+    <a href="lpo_print_obat.php?id=<?= $_GET['id'] ?>&inap&tgl=<?= $_GET['tgl'] ?>&tglObat=<?= $_GET['tglObat'] ?>&jenis=<?= $_GET['jenis'] ?>">Default</a>
+</div>
 <div style="max-width: 58mm; padding: 2mm;">
     <center>
         <img src="https://simkhm.id/wonorejo/admin/dist/assets/img/3.png" style="width: 25%; margin-bottom: 10px; margin-bottom: 0px;" alt="">
-        <h5>Obat Inap <?= $getPetugasSingle['obat_igd']?> Tanggal <?= $tglObat = htmlspecialchars($_GET['tglObat']) ?></h5>
+        <h5>Obat Inap <?= $getPetugasSingle['obat_igd'] ?> Tanggal <?= $tglObat = htmlspecialchars($_GET['tglObat']) ?></h5>
     </center>
     <table>
         <thead>
@@ -48,26 +59,40 @@ $getPetugasSingle = $koneksi->query("SELECT * FROM obat_rm  WHERE idrm = '$_GET[
         </tbody>
     </table>
     <hr style="margin-bottom: 0px;">
-    <div style="font-size: 12px; margin-bottom: 10px;">
-        <?php
-        $total = 0;
-        $getData = $koneksi->query("SELECT * FROM obat_rm  WHERE idrm = '$_GET[id]' AND tgl_pasien='$_GET[tgl]' AND DATE_FORMAT(created_at, '%Y-%m-%d') = '$_GET[tglObat]' AND obat_igd = '$_GET[jenis]'");
-        foreach ($getData as $data) {
-        ?>
+    <div>
+        <div style="font-size: 12px; margin-bottom: 10px;">
             <?php
-            $getPriceInDate = $koneksi->query("SELECT * FROM apotek WHERE tgl_beli <= '" . date('Y-m-d', strtotime($data['created_at'])) . "' AND nama_obat = '$data[nama_obat]' ORDER BY tgl_beli DESC LIMIT 1")->fetch_assoc();
+            $total = 0;
+            $getData = $koneksi->query("SELECT * FROM obat_rm  WHERE idrm = '$_GET[id]' AND tgl_pasien='$_GET[tgl]' AND DATE_FORMAT(created_at, '%Y-%m-%d') = '$_GET[tglObat]' AND obat_igd = '$_GET[jenis]'");
+            foreach ($getData as $data) {
             ?>
-            <?php number_format($harga = $getPriceInDate['harga_beli'] * ($getPriceInDate['margininap'] / 100), 0, 0, '.') ?>
-            <li style="margin-bottom: 5px;">
-                <?= $data['nama_obat'] ?> <?= $data['jml_dokter'] ?><br>
-                <?= $data['jml_dokter'] ?> x Rp<?= number_format($harga, 0, 0, '.') ?> : Rp <?= number_format($harga * $data['jml_dokter'], 0, 0, '.') ?>
-                <?php $total += $harga * $data['jml_dokter'] ?>
-            </li>
-        <?php } ?>
+                <?php
+                $getPriceInDate = $koneksi->query("SELECT * FROM apotek WHERE tgl_beli <= '" . date('Y-m-d', strtotime($data['created_at'])) . "' AND nama_obat = '$data[nama_obat]' ORDER BY tgl_beli DESC LIMIT 1")->fetch_assoc();
+                ?>
+                <?php number_format($harga = $getPriceInDate['harga_beli'] * ($getPriceInDate['margininap'] / 100), 0, 0, '.') ?>
+                <li style="margin-bottom: 5px;">
+                    <?= $data['nama_obat'] ?> (<b><?= $data['jml_dokter'] ?></b>)<br>
+                    <?php if(isset($_GET['eresep']) OR !isset($_GET['kasir'])){?>
+                        <?php if($data['petunjuk_obat'] != ''){?>
+                            <span style="text-transform: capitalize;">
+                                <?= $data['petunjuk_obat'] ?> <br>
+                            </span>
+                        <?php }?>
+                        <?= $data['dosis1_obat'] ?>x<?= $data['dosis2_obat'] ?> <?= $data['per_obat'] ?> (<?= $data['durasi_obat'] ?> Hari)<br>
+                    <?php }?>
+                    <?php if(isset($_GET['kasir']) OR !isset($_GET['eresep'])){?>
+                        <?= $data['jml_dokter'] ?> x Rp<?= number_format($harga, 0, 0, '.') ?> : Rp <?= number_format($harga * $data['jml_dokter'], 0, 0, '.') ?>
+                        <?php $total += $harga * $data['jml_dokter'] ?>
+                    <?php }?>
+                </li>
+            <?php } ?>
+        </div>
+        <hr>
+        <?php if(isset($_GET['kasir']) OR !isset($_GET['eresep'])){?>
+            <b style="margin-top: 10px;">Total : Rp <?= number_format($total, 0, 0, '.') ?></b><br>
+            Petugas : <?= $getPetugasSingle['petugas'] == '' ? $petugas : $getPetugasSingle['petugas'] ?>
+        <?php }?>
     </div>
-    <hr>
-    <b style="margin-top: 10px;">Total : Rp <?= number_format($total, 0, 0, '.') ?></b><br>
-    Petugas : <?= $getPetugasSingle['petugas'] == '' ? $petugas : $getPetugasSingle['petugas'] ?>
 </div>
 <script>
     window.print();
